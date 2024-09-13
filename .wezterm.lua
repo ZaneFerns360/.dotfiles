d765d76dc568c43d9b8f5bce2dbf9742321a6d2e
ZaneFerns360 <zane.ferns360@gmail.com>
2024-09-13 21:47:24 +0530

    chnages

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,35 @@ config.window_decorations = "NONE"
 config.window_background_opacity = 0.85
 config.initial_rows = 45
 config.initial_cols = 192
+config.text_background_opacity = 1
+
+config.colors = {
+	cursor_bg = "#F8F8F2",
+	cursor_border = "#F8F8F2",
+	cursor_fg = "#011423",
+	selection_bg = "#44475A",
+	selection_fg = "#F8F8F2",
+	ansi = {
+		"#000000", -- Black
+		"#cb0302", -- Red
+		"#18ca00", -- Green
+		"#cdca00", -- Yellow
+		"#0c72cb", -- Blue
+		"#ca1dd0", -- Purple (Magenta)
+		"#0ccccc", -- Cyan
+		"#dddddd", -- White
+	},
+	brights = {
+		"#757575", -- Bright Black (Gray)
+		"#f21f1e", -- Bright Red
+		"#22fd00", -- Bright Green
+		"#fffd00", -- Bright Yellow
+		"#198fff", -- Bright Blue
+		"#fd27ff", -- Bright Purple
+		"#13ffff", -- Bright Cyan
+		"#ffffff", -- Bright White
+	},
+}
 
 config.window_padding = {
 	left = 0,
